7f50771cf36abdd1f8d892177417a24c718332b7
Novi <novifitriaramadhan@gmail.com>
2024-11-11 00:02:17 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const { addBookHandler, getAllBooksHandler, getBooksByIdHandler, updateBookByIdHandler, deleteBookByIdHandler } = require("./handler");
+
+const routes = [
+    {
+      method: 'POST',
+      path: '/books',
+      handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooksHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{id}',
+        handler: getBooksByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{id}',
+        handler: updateBookByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: deleteBookByIdHandler,
+    },
+];
+   
+module.exports = routes;
