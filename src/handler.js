09f7dcb9a8a067b4cd4706981a0659405b6d52e2
Novi <novifitriaramadhan@gmail.com>
2024-11-11 00:26:14 +0700

    initial commit

--- src/handler.js
+++ src/handler.js
@@ -4,24 +4,6 @@ const books = require('./books');
 const addBookHandler = (request, h) => {
     const {name, year, author, summary, publisher, pageCount, readPage, reading} = request.payload;
 
-    if (!name) {
-        const response = h.response({
-            status: 'fail',
-            message: 'Gagal menambahkan buku. Mohon isi nama buku',
-        });
-        response.code(400);
-        return response;
-    }
-    
-    if (readPage > pageCount) {
-        const response = h.response ({
-            status: 'fail',
-            message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
-        });
-        response.code(400);
-        return response;
-    }
-
     const id = nanoid(16);
     const finished = pageCount === readPage;
     const insertedAt = new Date().toISOString();
@@ -41,6 +23,24 @@ const addBookHandler = (request, h) => {
             data: { bookId: id },
         });
         response.code(201);
+        return response;
+    }
+
+    if (!name) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. Mohon isi nama buku',
+        });
+        response.code(400);
+        return response;
+    }
+    
+    if (readPage > pageCount) {
+        const response = h.response ({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+        });
+        response.code(400);
         return response;
     }
     
